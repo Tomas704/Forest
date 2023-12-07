f6369ea2d3e9e94db778b2ac528edc70e7f0196d
Tomas704 <kamensky.tomas704@gmail.com>
2023-12-07 20:54:17 +0100

    Added hello word.

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace Forest
             {
                 Console.WriteLine(tree.GetInfo());
             }
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
         }
     }
 
